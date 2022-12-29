7f7edf36a31a39fea72b4198113f97264666da4d
stephen0603 <stephen850603@gmail.com>
2022-12-29 17:06:25 +0800

    update build

--- coffee/dist/js/about.be8fba47.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkcoffee"]=self["webpackChunkcoffee"]||[]).push([[443],{7381:function(e,n,u){u.r(n),u.d(n,{default:function(){return l}});var t=u(3396);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),r=[c];function s(e,n){return(0,t.wg)(),(0,t.iD)("div",a,r)}var f=u(89);const o={},i=(0,f.Z)(o,[["render",s]]);var l=i}}]);
-//# sourceMappingURL=about.be8fba47.js.map
